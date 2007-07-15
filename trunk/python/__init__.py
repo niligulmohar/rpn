8c3b0f5ae3fd253f7c3deb8bc5529a1b6afd2827
nili <nili@e54a3c0e-4035-0410-a484-a8ba4a46ab34>
2007-07-15 18:42:23 +0000

    Fungerande operatorer tillagda.
    git-svn-id: file:///home/nili/repos-0-190/rpn@6 e54a3c0e-4035-0410-a484-a8ba4a46ab34

--- trunk/python/__init__.py
+++ trunk/python/__init__.py
@@ -24,6 +24,8 @@ def index_directory(none, directory, filenames):
 
 os.path.walk('data', index_directory, None)
 
+######################################################################
+
 class Texture(object):
     @classmethod
     def class_init(cls):
@@ -31,8 +33,8 @@ class Texture(object):
             return game.py.font.Font(files[name + '.TTF'], size)
         cls.font = make_font('TEACPSSB', 8)
         cls.texts = {}
-    @classmethod
     # TODO: Låta bli att läcka text_surface:s
+    @classmethod
     def text(cls, text):
         if cls.texts.has_key(text):
             return cls.texts[text]
@@ -40,7 +42,6 @@ class Texture(object):
         texture_surface = game.py.Surface((2 ** int(math.ceil(math.log(small_surface.get_width()) / math.log(2))),
                                            2 ** int(math.ceil(math.log(small_surface.get_height()) / math.log(2)))),
                                           game.py.SRCALPHA, 32)
-        #texture_surface.fill((0,128,0,128))
         texture_surface.blit(small_surface, (0,0))
         texture = Texture(surface = texture_surface)
         texture.text_width = float(small_surface.get_width() - 1) / texture_surface.get_width()
@@ -80,12 +81,12 @@ class Model(object):
         self.frames = 0
         self.grid = Grid()
         self.robots = [Robot(self.grid)]
-        self.draw("""
+        self.draw_map("""
   # #
 #############
-#0123456789 #
+#+-*/       #
 #    ?      #
-#   #   *   #
+#   #   S   #
 #  !#5    1 #
 # #####     #
 #  7#?  # # #
@@ -97,7 +98,7 @@ class Model(object):
     def before_frame(self):
         self.frames += 1
         self.robots[0].act_on_inputs(self.move_up, self.move_down, self.move_left, self.move_right, self.action)
-    def draw(self, string):
+    def draw_map(self, string):
         lines = string.split("\n")
         y0 = -len(lines) / 2
         x0 = -max((len(s) for s in lines)) / 2 + 1
@@ -106,19 +107,24 @@ class Model(object):
                 x = x0+xd
                 y = y0+yd
                 self.grid.set(x, y, Cell(c == '#'))
-                if c == '*':
+                if c == 'S':
                     self.robots[0].move(x, y)
                 elif c in "0123456789":
                     self.grid.get(x, y).object = Number(self.grid, ord(c) - ord("0"))
                 elif c == '?':
                     self.grid.get(x, y).object = Number(self.grid, random.randrange(100))
                 elif c == '!':
-                    self.grid.get(x, y).object = Number(self.grid, random.randrange(10000))
+                    self.grid.get(x, y).object = Number(self.grid, random.randrange(1000))
+                elif c in "+-*/":
+                    self.grid.get(x, y).object = Operator(self.grid, c)
+                    
                     
 class Cell(object):
     def __init__(self, blocked = False):
         self.blocked = blocked
         self.object = None
+    def has_action(self):
+        return not self.blocked
 
 class Grid(object):
     def __init__(self):
@@ -168,6 +174,7 @@ def clamp(min_val, x, max_val):
     return max(min_val, min(x, max_val))
 
 class Robot(Entity):
+    MAX_STACK_HEIGHT = 11
     def __init__(self, grid):
         Entity.__init__(self, grid)
         self.target_x = 0
@@ -243,15 +250,21 @@ class Robot(Entity):
 
         if action.get_triggered():
             target = self.grid.get(self.target_x, self.target_y)
-            if target.blocked:
+            if not target.has_action():
                 pass
             elif target.object:
-                self.stack.append(target.object)
-                target.object = None
-            elif len(self.stack) > 0:
+                if not self.stack_full():
+                    self.stack.append(target.object)
+                    target.object.pushed_on(self.stack)
+                    target.object = None
+            elif not self.stack_empty():
                 target.object = self.stack.pop()
             else:
                 pass
+    def stack_full(self):
+        return len(self.stack) >= self.MAX_STACK_HEIGHT
+    def stack_empty(self):
+        return len(self.stack) == 0
 
 class Number(Entity):
     def __init__(self, grid, numerator, denominator = 1):
@@ -259,10 +272,46 @@ class Number(Entity):
         self.numerator = numerator
         self.denominator = denominator
     def text_len(self):
-        return len("%d" % self.numerator)
+        return len(str(self.numerator))
+    def pushed_on(self, stack):
+        pass
+
+class Operator(Entity):
+    def __init__(self, grid, operator_type = "+"):
+        Entity.__init__(self, grid)
+        self.operator_type = operator_type
+    def text_len(self):
+        return len(self.operator_type)
+    def pushed_on(self, stack):
+        if len(stack) >= 3:
+            stack.pop()
+            operand0 = stack.pop()
+            operand1 = stack.pop()
+            if self.operator_type == '+':
+                stack.append(Number(self.grid, operand1.numerator + operand0.numerator))
+            elif self.operator_type == '-':
+                stack.append(Number(self.grid, operand1.numerator - operand0.numerator))
+            elif self.operator_type == '*':
+                stack.append(Number(self.grid, operand1.numerator * operand0.numerator))
+            elif self.operator_type == '/':
+                stack.append(Number(self.grid, float(operand1.numerator) / operand0.numerator))
 
 
 ######################################################################
+
+class DampedValue(object):
+    def __init__(self, value, difference_reduction = 1.0/13):
+        self.set_immediately(value)
+        self.difference_reduction = difference_reduction
+    def set_target(self, value):
+        self.target = value
+    def set_immediately(self, value):
+        self.current = value
+        self.target = value
+    def update(self):
+        self.current += (self.target - self.current) * self.difference_reduction
+    def __call__(self):
+        return self.current
 
 class BodyPart(object):
     @classmethod
@@ -273,7 +322,6 @@ class BodyPart(object):
             cls.sprites[n].outline = Texture(outline_of = cls.sprites[n])
 
     def __init__(self, texture, color, parent = None, outline = True):
-        self.animation_speed_factor = 1.0/13
         self.visible = True
         self.outline = outline
         self.size = 16
@@ -282,11 +330,9 @@ class BodyPart(object):
         self.color = color
         self.outline_color = [0,0,0,1]
         self.pos0 = [0,0]
-        self.pos = [0,0]
-        self.current_pos = self.pos[:]
+        self.pos = [DampedValue(0), DampedValue(0)]
         self.rot0 = 0
-        self.rot = 0
-        self.current_rot = self.rot
+        self.rot = DampedValue(0)
         self.rot_factor = 1
         self.children = []
         if parent:
@@ -298,11 +344,12 @@ class BodyPart(object):
         if self.visible and ((layer == 0 and self.outline) or layer == 1):
             glMatrixMode(GL_MODELVIEW)
             glPushMatrix()
-            self.current_pos[0] += (self.pos[0] - self.current_pos[0]) * self.animation_speed_factor
-            self.current_pos[1] += (self.pos[1] - self.current_pos[1]) * self.animation_speed_factor
-            self.current_rot += (self.rot - self.current_rot) * self.animation_speed_factor
-            glTranslatef(self.pos0[0] + self.current_pos[0], self.pos0[1] + self.current_pos[1], 0)
-            glRotatef(self.rot0 + self.current_rot * self.rot_factor, 0, 0, 1)
+            self.pos[0].update()
+            self.pos[1].update()
+            self.rot.update()
+
+            glTranslatef(self.pos0[0] + self.pos[0](), self.pos0[1] + self.pos[1](), 0)
+            glRotatef(self.rot0 + self.rot() * self.rot_factor, 0, 0, 1)
     
             c, t = ((self.outline_color, self.outline_texture),
                     (self.color, self.texture))[layer]
@@ -348,21 +395,21 @@ class RobotSprite(object):
         self.eye_l = BodyPart(BodyPart.sprites['eye1'], [1,1,1,1], self.head, False)
         self.eye_l.pos0 = [-4, -7]
         self.pupil_l = BodyPart(BodyPart.sprites['pupil1'], [0,0,0,1], self.eye_l, False)
-        self.pupil_l.animation_speed_factor = 0.5
+        self.pupil_l.pos[0].difference_reduction = 0.5
+        self.pupil_l.pos[1].difference_reduction = 0.5
         self.eye_r = BodyPart(BodyPart.sprites['eye1'], [1,1,1,1], self.head, False)
         self.eye_r.pos0 = [4, -7]
         self.pupil_r = BodyPart(BodyPart.sprites['pupil1'], [0,0,0,1], self.eye_r, False)
-        self.pupil_r.animation_speed_factor = 0.5
+        self.pupil_r.pos[0].difference_reduction = 0.5
+        self.pupil_r.pos[1].difference_reduction = 0.5
         self.arm_r = BodyPart(BodyPart.sprites['arm1'], color, self.body)
         self.arm_r.pos0 = [6, -1]
-        self.arm_r.rot = 30
-        self.arm_r.current_rot = 30
+        self.arm_r.rot.set_immediately(30)
         self.arm_l = BodyPart(BodyPart.sprites['arm1'], color, self.body)
         self.arm_l.pos0 = [-6, -1]
         self.arm_l.rot0 = 180
         self.arm_l.rot_factor = -1
-        self.arm_l.rot = 30
-        self.arm_l.current_rot = 30
+        self.arm_l.rot.set_immediately(30)
         self.leg_r = BodyPart(BodyPart.sprites['leg_r1'], color, self.body)
         self.leg_r.pos0 = [4,4]
         self.leg_l = BodyPart(BodyPart.sprites['leg_l1'], color, self.body)
@@ -408,16 +455,22 @@ class RobotSprite(object):
         else:
             arm_angle = 30
             torso_height = 0
-        self.arm_r.rot = self.arm_l.rot = arm_angle
-        self.body.pos[1] = torso_height
-        self.leg_l.pos[0] = sideways_l
-        self.leg_r.pos[0] = sideways_r
-        self.leg_l.pos[1] = -torso_height + height_l
-        self.leg_r.pos[1] = -torso_height + height_r
-        self.shadow.pos[1] = -torso_height
+        self.arm_r.rot.set_target(arm_angle)
+        self.arm_l.rot.set_target(arm_angle)
+        self.body.pos[1].set_target(torso_height)
+        self.leg_l.pos[0].set_target(sideways_l)
+        self.leg_r.pos[0].set_target(sideways_r)
+        self.leg_l.pos[1].set_target(-torso_height + height_l)
+        self.leg_r.pos[1].set_target(-torso_height + height_r)
+        self.shadow.pos[1].set_target(-torso_height)
         if self.moving:
-            self.pupil_l.pos = self.pupil_r.pos = [self.entity.dx * self.PUPIL_FACTOR,
-                                                   self.entity.dy * self.PUPIL_FACTOR]
+            x = self.entity.dx * self.PUPIL_FACTOR
+            self.pupil_l.pos[0].set_target(x)
+            self.pupil_r.pos[0].set_target(x)
+            y = self.entity.dy * self.PUPIL_FACTOR
+            self.pupil_l.pos[1].set_target(y)
+            self.pupil_r.pos[1].set_target(y)
+
         elif self.frame >= self.look_delay:
             self.new_look_delay()
             if random.random() > 0.1:
@@ -426,17 +479,19 @@ class RobotSprite(object):
                 y = math.sin(angle) * 2
             else:
                 x = y = 0
-            self.pupil_l.pos = [x, y]
-            self.pupil_r.pos = [x, y]
+            self.pupil_l.pos[0].set_target(x)
+            self.pupil_l.pos[1].set_target(y)
+            self.pupil_r.pos[0].set_target(x)
+            self.pupil_r.pos[1].set_target(y)
         if self.frame >= self.gesture_delay:
             self.new_gesture_delay()
             self.dance = not self.dance
         if self.entity.dy < 0:
             self.eye_r.visible = False
             self.eye_l.visible = False
-            self.head.pos[1] = 2
+            self.head.pos[1].set_target(2)
         else:
-            self.head.pos[1] = 0
+            self.head.pos[1].set_target(0)
             if self.frame >= self.blink_delay:
                 self.new_blink_delay()
                 self.eye_r.visible = True
@@ -454,23 +509,32 @@ class NumberSprite(object):
     @classmethod
     def class_init(cls):
         cls.sprites = {}
-        for n in ['number_base']:
+        for n in ['number_base', 'operator_base']:
             cls.sprites[n] = Texture(filename = n + '.png')
     @classmethod
-    def color(cls, number):
-        return [(0.3, 0.3, 0.3),
-                (0.5, 0.3, 0.0),
-                (0.8, 0.0, 0.0),
-                (1.0, 0.5, 0.0),
-                (0.9, 0.9, 0.0),
-                (0.0, 0.8, 0.0),
-                (0.0, 0.0, 0.9),
-                (1.0, 0.0, 1.0),
-                (0.5, 0.5, 0.5),
-                (0.9, 0.9, 0.9)][number.numerator % 10]
+    def color(cls, obj):
+        if isinstance(obj, Operator):
+            return (random.expovariate(13),
+                    random.expovariate(17),
+                    random.expovariate(13))
+        else:
+            return [(0.3, 0.3, 0.3),
+                    (0.5, 0.3, 0.0),
+                    (0.8, 0.0, 0.0),
+                    (1.0, 0.5, 0.0),
+                    (0.9, 0.9, 0.0),
+                    (0.0, 0.8, 0.0),
+                    (0.0, 0.0, 0.9),
+                    (1.0, 0.0, 1.0),
+                    (0.5, 0.5, 0.5),
+                    (0.9, 0.9, 0.9)][abs(int(obj.numerator % 10))]
     @classmethod
     def draw(cls, obj):
-        NumberSprite.sprites['number_base'].bind()
+        operator = isinstance(obj, Operator)
+        if operator:
+            NumberSprite.sprites['operator_base'].bind()
+        else:
+            NumberSprite.sprites['number_base'].bind()
         glBegin(GL_QUADS)
         glColor3f(*NumberSprite.color(obj))
         glTexCoord2f(0, 0)
@@ -482,7 +546,10 @@ class NumberSprite(object):
         glTexCoord2f(0, 1)
         glVertex2f(-1, 1)
         glEnd()
-        t = Texture.text("%d" % obj.numerator)
+        if operator:
+            t = Texture.text(obj.operator_type)
+        else:
+            t = Texture.text(str(obj.numerator))
         t.bind()
         glBegin(GL_QUADS)
         if obj.text_len() == 1:
@@ -583,11 +650,13 @@ class RpnView(game.View):
         self.robot = RobotSprite(model.robots[0], [0.6,0.2,0.2,1])
         self.map = Map(model.grid)
         self.center = [0,0]
-        self.zoom = 2
-        self.current_zoom = random.uniform(0.25, 8)
-        self.current_center = [random.uniform(-15, 15), random.uniform(-15, 15)]
-        self.fade_to_black = 0
-        self.current_fade_to_black = 1
+        self.zoom = DampedValue(random.uniform(0.25, 8), 0.05)
+        self.zoom.set_target(2)
+        self.center = [DampedValue(random.uniform(-15, 15), 0.05) for n in xrange(2)]
+        self.center[0].set_target(0)
+        self.center[1].set_target(0)
+        self.fade_to_black = DampedValue(1, 0.08)
+        self.fade_to_black.set_target(0)
 
     def update(self):
         glMatrixMode(GL_PROJECTION)
@@ -602,32 +671,44 @@ class RpnView(game.View):
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-        self.current_zoom += (self.zoom - self.current_zoom) * 0.05
-        self.current_center[0] += (self.center[0] - self.current_center[0]) * 0.05
-        self.current_center[1] += (self.center[1] - self.current_center[1]) * 0.05
+        self.zoom.update()
+        self.center[0].update()
+        self.center[1].update()
         glPushMatrix()
-        glScalef(*([self.current_zoom]*3))
-        glTranslatef(-self.current_center[0] * Map.TILE_SIZE, -self.current_center[1] * Map.TILE_SIZE, 0)
+        glScalef(*([self.zoom()]*3))
+        glTranslatef(-self.center[0]() * Map.TILE_SIZE,
+                     -self.center[1]() * Map.TILE_SIZE,
+                     0)
 
         glClearColor(*self.CLEAR_COLOR)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        x0 = int(-WIDTH / 2 / Map.TILE_SIZE / self.current_zoom + self.current_center[0]) - 1
-        x1 = int(WIDTH / 2 / Map.TILE_SIZE / self.current_zoom + self.current_center[0]) + 2
-        y0 = int(-HEIGHT / 2 / Map.TILE_SIZE / self.current_zoom + self.current_center[1]) - 1
-        y1 = int(HEIGHT / 2 / Map.TILE_SIZE / self.current_zoom + self.current_center[1]) + 2
+        x0 = int(-WIDTH / 2 / Map.TILE_SIZE / self.zoom() + self.center[0]()) - 1
+        x1 = int(WIDTH / 2 / Map.TILE_SIZE / self.zoom() + self.center[0]()) + 2
+        y0 = int(-HEIGHT / 2 / Map.TILE_SIZE / self.zoom() + self.center[1]()) - 1
+        y1 = int(HEIGHT / 2 / Map.TILE_SIZE / self.zoom() + self.center[1]()) + 2
         self.map.draw(x0,y0,x1,y1, self.model.frames)
         glPushMatrix()
         x, y = self.model.robots[0].target_x, self.model.robots[0].target_y
         Map.transform_for(x, y)
         glDisable(GL_TEXTURE_2D)
         glBegin(GL_QUAD_STRIP)
-        if self.model.grid.get(x, y).blocked:
+        cell = self.model.grid.get(x, y)
+        if cell.has_action():
+            alpha = 0.75 + math.sin(self.model.frames * 0.1) * 0.25
+            if cell.object:
+                if self.model.robots[0].stack_full():
+                    glColor4f(1,1,0,alpha)
+                else:
+                    glColor4f(0,1,0,alpha)
+            else:
+                if self.model.robots[0].stack_empty():
+                    glColor4f(0,0,0,alpha * 0.5)
+                else:
+                    glColor4f(0,1,1,alpha)
+        else:
             alpha = 0.75 + math.sin(self.model.frames) * 0.25
             glColor4f(1,0,0,alpha)
-        else:
-            alpha = 0.75 + math.sin(self.model.frames * 0.1) * 0.25
-            glColor4f(0,1,0,alpha)
         OUTER = 1
         INNER = 0.8
         glVertex2f(-OUTER, -OUTER)
@@ -644,20 +725,30 @@ class RpnView(game.View):
         glPopMatrix()
         glEnable(GL_TEXTURE_2D)
         self.robot.draw()
-        
-
 
         glPopMatrix()
-        self.current_fade_to_black += (self.fade_to_black - self.current_fade_to_black) * 0.02
-        if self.current_fade_to_black > 0.01:
+
+        glPushMatrix()
+        glTranslate(-WIDTH * 0.45, HEIGHT * 0.425, 0)
+        glScale(*([Map.TILE_SIZE]*3))
+        for number in self.model.robots[0].stack:
+            NumberSprite.draw(number)
+            glTranslate(0, -2, 0)
+        glPopMatrix()
+
+        glScale(*([2]*3))
+
+        self.fade_to_black.update()
+        if self.fade_to_black() > 0.01:
             glDisable(GL_TEXTURE_2D)
             glBegin(GL_QUADS)
-            glColor4f(0,0,0,self.current_fade_to_black)
+            glColor4f(0,0,0,self.fade_to_black())
             glVertex2f(-WIDTH/2, -HEIGHT/2)
             glVertex2f(WIDTH/2, -HEIGHT/2)
             glVertex2f(WIDTH/2, HEIGHT/2)
             glVertex2f(-WIDTH/2, HEIGHT/2)
             glEnd()
+
         game.py.display.flip()
 
 
